44895d080195013754ae6b3bf1d51c4150f99c67
knucker <you@example.com>
2023-05-27 20:17:27 +0300

    remove target

--- target/debug/deps/sonic-863e570ce90bc421.d
+++
@@ -1,6 +0,0 @@
-/home/knucker/sonic/target/debug/deps/sonic-863e570ce90bc421: src/main.rs src/pool.rs
-
-/home/knucker/sonic/target/debug/deps/sonic-863e570ce90bc421.d: src/main.rs src/pool.rs
-
-src/main.rs:
-src/pool.rs:
--- target/debug/deps/sonic-a4d277143a654bd6.d
+++
@@ -1,6 +0,0 @@
-/home/knucker/sonic/target/debug/deps/sonic-a4d277143a654bd6.rmeta: src/main.rs src/pool.rs
-
-/home/knucker/sonic/target/debug/deps/sonic-a4d277143a654bd6.d: src/main.rs src/pool.rs
-
-src/main.rs:
-src/pool.rs:

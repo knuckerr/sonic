7c86014760b1cfe83c9ad9eef325828c3e7a52af
knucker <you@example.com>
2023-06-02 01:32:14 +0300

    [Added] make the server use shards

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -6,29 +6,37 @@ use std::error::Error;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use std::sync::{Arc, Mutex};
-const BUFFER_SIZE: usize = 1024;
 use crate::server::parser::{parse_command, Command};
 
 pub struct Server {
     thread_pool: thread_pool::RayonThreadPool,
+    buffer_size: usize,
+    shard_count: usize,
 }
 impl Server {
     pub fn new() -> Self {
         let _thread_pool = thread_pool::RayonThreadPool::new(num_cpus::get() as u32).unwrap();
         Server {
             thread_pool: _thread_pool,
+            buffer_size: 1024,
+            shard_count: 4,
         }
+    }
+    pub fn create_shards(&self) -> Vec<Arc<Mutex<store::Store>>> {
+        let mut shards = Vec::with_capacity(self.shard_count);
+        for _ in 0..self.shard_count {
+            shards.push(Arc::new(Mutex::new(store::Store::new())));
+        }
+        shards
     }
     pub fn run<A: ToSocketAddrs>(self, addr: A) -> Result<(), Box<dyn Error>> {
         let listener = TcpListener::bind(addr)?;
-        let memory_pool = Arc::new(Mutex::new(memory_pool::MemoryPool::new()));
-        let data = Arc::new(Mutex::new(store::Store::new()));
+        let shards = self.create_shards();
         for stream in listener.incoming() {
-            let pool = Arc::clone(&memory_pool);
-            let data = Arc::clone(&data);
+            let shards = shards.clone();
             self.thread_pool.spawn(move || match stream {
                 Ok(stream) => {
-                    if let Err(e) = handle_client(data, pool, stream) {
+                    if let Err(e) = handle_client(&shards, self.buffer_size, stream) {
                         println!("{}", e)
                     }
                 }
@@ -40,52 +48,41 @@ impl Server {
 }
 
 fn handle_client(
-    data: Arc<Mutex<store::Store>>,
-    pool: Arc<Mutex<memory_pool::MemoryPool>>,
+    shards: &[Arc<Mutex<store::Store>>],
+    buffer_size: usize,
     stream: TcpStream,
 ) -> Result<(), Box<dyn Error>> {
+    let mut buffer = Vec::with_capacity(buffer_size);
+
     let mut reader = stream.try_clone()?;
     let mut writer = stream.try_clone()?;
 
-    let mut buffer = {
-        let mut pool = pool.lock().expect("Failed to acquire lock on pool");
-        let ptr = pool.allocate(BUFFER_SIZE);
-        unsafe { std::slice::from_raw_parts_mut(ptr, BUFFER_SIZE) }
-    };
-
     loop {
-        let size = reader.read(buffer)?;
+        buffer.clear();
+        let size = reader.read(&mut buffer)?;
         let command = String::from_utf8_lossy(&buffer[..size]);
+
         let response = match parse_command(&command) {
             Command::Quit => break,
-            cmd => {
-                handle_command(cmd, &data)
-            }
+            cmd => handle_command(cmd, shards),
         };
+
         writer.write_all(&response)?;
         writer.flush()?;
-        let mut pool = pool.lock().expect("Failed to acquire lock on pool");
-        let ptr = buffer.as_mut_ptr();
-        pool.deallocate(ptr);
-        let new_ptr = pool.allocate(BUFFER_SIZE);
-        buffer = unsafe { std::slice::from_raw_parts_mut(new_ptr, BUFFER_SIZE) };
     }
-    let mut pool = pool.lock().expect("Failed to acquire lock on pool");
-    let ptr = buffer.as_mut_ptr();
-    pool.deallocate(ptr);
+
     Ok(())
 }
 
-fn handle_command(command: Command, data: &Arc<Mutex<store::Store>>) -> Vec<u8> {
-    let mut locked_data = data.lock().expect("failed to acquire lock on data");
+fn handle_command(command: Command, shards: &[Arc<Mutex<store::Store>>]) -> Vec<u8> {
+    let shard_index = get_shard_index(&command, shards.len());
+    let locked_data = &mut shards[shard_index]
+        .lock()
+        .expect("failed to acquire lock on data");
     match command {
-        Command::Get(key) => {
-            let value = locked_data.get(key);
-            match value {
-                Some(v) => v,
-                None => b"Key not found".to_vec(),
-            }
-        }
+        Command::Get(key) => locked_data
+            .get(key)
+            .map_or_else(|| b"Key not found".to_vec(), |v| v.clone()),
         Command::Set(key, value) => {
             locked_data.set(key, value);
             b"Set".to_vec()
@@ -96,4 +93,20 @@ fn handle_command(command: Command, data: &Arc<Mutex<store::Store>>) -> Vec<u8> 
         }
         _ => b"test".to_vec(),
     }
+}
+
+fn get_shard_index(command: &Command, shard_count: usize) -> usize {
+    match command {
+        Command::Get(key) | Command::Del(key) => hash_key(key) % shard_count,
+        Command::Set(key, _) => hash_key(key) % shard_count,
+        _ => 0,
+    }
+}
+
+fn hash_key(key: &str) -> usize {
+    let mut hash = 0;
+    for byte in key.bytes() {
+        hash = (hash + byte as usize) % usize::MAX
+    }
+    hash
 }
